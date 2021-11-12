302834858b3112f2d146c54eb254205ce102df3f
koomekevo <koome.kevo@gurucrafts.net>
2021-11-12 11:58:08 +0300

    Create portfolio bugs repository

---
+++ modules/menuWindow.js
@@ -0,0 +1,14 @@
+const menu = document.querySelector('.menu-dropdown');
+
+// Confirmed open-menu working well
+function openMenu() {
+  menu.style.visibility = 'visible';
+}
+
+// Confirmed close-menu working well
+function closeMenu() {
+  menu.style.visibility = 'hidden';
+}
+
+openMenu();
+closeMenu();
